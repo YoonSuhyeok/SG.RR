df10376e83297ccc0ae9aa5ebf714b1a0fb10609
Melona-BS <han9119693@naver.com>
2021-01-06 16:05:13 +0900

    Day1 - Button & UI

--- app/src/main/java/com/sg/sgrr/MainActivity.kt
+++ app/src/main/java/com/sg/sgrr/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.sg.sgrr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.record_result_activity)
 
         // 안드로이드
 
